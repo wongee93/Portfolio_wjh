4051857a98694c318230e1d71badd9bf98372008
wongee93 <jhwon9304@gmail.com>
2023-10-04 18:40:50 +0900

    css 틀 잡기

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <nav className='nav_bar'>App header</nav>
-      <div className='container_wrapper'>
+      <nav className='nav-bar'>
+        <div className='app-header-global'></div>
+        <div className='app-header-local'></div>
+      </nav>
+      <div className='main'>
         <div className='container'>
-          container
+          <div className='data-view-container'>
+            <div className='layout-sidebar'></div>
+            <div className='layout-main'></div>
+          </div>
         </div>
       </div>
     </div>
