f70aaf68e2e4128dbaedc8e8999fcf3290068936
wongee93 <jhwon9304@gmail.com>
2023-10-11 22:18:07 +0900

    edit timetable wrapper css style

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -7,14 +7,21 @@ const Overview = () => {
 
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    const SquareSpan = () => <span></span>;
+    // const SquareSpan = () => <span></span>;
+    const SquareSpan = ({ color }) => <span style={{ backgroundColor: color }}></span>;
+
 
     const rows = 7;
     const columns = 52;
     const spans = [];
 
+    // for (let i = 0; i < rows * columns; i++) {
+    //     spans.push(<SquareSpan key={i} />);
+    // }
     for (let i = 0; i < rows * columns; i++) {
-        spans.push(<SquareSpan key={i} />);
+        const currentColumn = i % columns;
+        const color = (currentColumn >= 26 && currentColumn <= 38) ? '#40c463' : ''; // 27번째부터 43번째 column에 빨간색을 지정
+        spans.push(<SquareSpan key={i} color={color} />);
     }
 
     return (
@@ -61,16 +68,19 @@ const Overview = () => {
                 <div className='timeline-box'>
                     <div className='overview-title'>Timeline</div>
                     <div className='timeline-wrapper'>
-                        <table className='timeline-table'>
-                            <thead className='timeline-month'>
-                                <tr>
-                                    {months.map(month => <th key={month}>{month}</th>)}
-                                </tr>
-                            </thead>
-                            <tbody className='timeline'>
-                                {spans}
-                            </tbody>
-                        </table>
+                        <div className='timetable-wrapper'>
+                            <table className='timeline-table'>
+                                <thead className='timeline-month'>
+                                    <tr>
+                                        {months.map(month => <th key={month}>{month}</th>)}
+                                    </tr>
+                                </thead>
+                                <tbody className='timeline'>
+                                    {spans}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className=''>test</div>
                     </div>
                 </div>
             </div>
