2d8835d94bcdb56ff202213deb92968cb6fe3793
wongee93 <jhwon9304@gmail.com>
2023-10-11 23:38:07 +0900

    add 2022, 2023 timetable history

--- src/component/TimeTable.js
+++ src/component/TimeTable.js
@@ -2,21 +2,57 @@ const TimeTable = () => {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     // const SquareSpan = () => <span></span>;
-    const SquareSpan = ({ color }) => <span style={{ backgroundColor: color }}></span>;
+    // const SquareSpan = ({ color }) => <span style={{ backgroundColor: color }}></span>;
 
+    const SquareSpan = ({ type }) => {
+        if (type === 'split') {
+            return (
+                <span className='split-colors'>
+                    <div className='half-color first-color'></div>
+                    <div className='half-color second-color'></div>
+                </span>
+            );
+        }
+        return <span style={{ backgroundColor: type }}></span>;
+    };
 
     const rows = 7;
     const columns = 52;
-    const spans = [];
 
-    // for (let i = 0; i < rows * columns; i++) {
-    //     spans.push(<SquareSpan key={i} />);
-    // }
+    // 2023
+    const spans_2023 = [];
+
     for (let i = 0; i < rows * columns; i++) {
         const currentColumn = i % columns;
-        const color = (currentColumn >= 26 && currentColumn <= 38) ? '#40c463' : ''; // 27번째부터 43번째 column에 빨간색을 지정
-        spans.push(<SquareSpan key={i} color={color} />);
+        let type = '';
+
+        if ((currentColumn >= 0 && currentColumn <= 12) || (currentColumn >= 39 && currentColumn <= 40)) { // 첫 12개의 column에 #407ec4 색상 지정
+            type = '#4a91e2';
+        } else if (currentColumn >= 26 && currentColumn <= 38) {
+            type = '#40c463'; // 27번째부터 43번째 column에 #40c463 색상 지정
+        }
+
+        spans_2023.push(<SquareSpan key={i} type={type} />);
     }
+
+    // 2022
+    const spans_2022 = [];
+
+    for (let i = 0; i < rows * columns; i++) {
+        const currentColumn = i % columns;
+        let type = '';
+
+        if (currentColumn <= 23) {
+            type = '#40c463';
+        } else if (currentColumn >= 44) {
+            type = 'split';
+        } else if (currentColumn >= 24 && currentColumn < 44) {
+            type = '#e2be49';
+        }
+
+        spans_2022.push(<SquareSpan key={i} type={type} />);
+    }
+
     return (
         <div className='timeline-box'>
             <div className='overview-title'>Timeline</div>
@@ -29,7 +65,7 @@ const TimeTable = () => {
                             </tr>
                         </thead>
                         <tbody className='timeline'>
-                            {spans}
+                            {spans_2023}
                         </tbody>
                     </table>
                 </div>
