5632b0b118b6d8d60452387065179a7d295987fa
wongee93 <jhwon9304@gmail.com>
2023-11-02 22:10:31 +0900

    rearrange activity-wrapper elements

---
+++ src/component/HistoryTable.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const HistoryTable = () => {
+
+    // const [selectedYear, setSelectedYear] = useState(true);
+
+    // const handleYearButtonClick = (year) => {
+    //     setSelectedYear(year);
+    // };
+
+    return (
+        <div className='history-table-box'>
+            <div className='overview-title'>Timeline Activity</div>
+            <div className='history-table-wrapper'>
+            </div>
+        </div>
+    )
+}
+
+export default HistoryTable;
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LayoutSidebar from '../component/LayoutSidebar';
 import TimeTable from '../component/TimeTable';
 import ProjectCard from '../component/ProjectCards';
+import HistoryTable from '../component/HistoryTable';
 
 const Overview = () => {
+
+    const [selectedYear, setSelectedYear] = useState(true);
+
+    const handleYearButtonClick = (year) => {
+        setSelectedYear(year);
+    };
 
     return (
         <>
             <LayoutSidebar />
             <div className='layout-main'>
                 <ProjectCard />
-                <TimeTable />
+                <div className='activity-wrapper'>
+                    <div className='activities'>
+                        <TimeTable />
+                        <HistoryTable />
+                    </div>
+                    <div className='year-btn-wrapper'>
+                        <span className={`year-btn ${selectedYear ? 'year-btn-on' : 'year-btn-off'}`}
+                            onClick={() => handleYearButtonClick(true)}>2023</span>
+                        <span className={`year-btn ${!selectedYear ? 'year-btn-on' : 'year-btn-off'}`}
+                            onClick={() => handleYearButtonClick(false)}>2022</span>
+                    </div>
+                </div>
             </div>
         </>
     )
